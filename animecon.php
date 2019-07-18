6195bded011e4a57847d2bf6b522162da867dbd4
tsitsu <48997669+tsitsu@users.noreply.github.com>
2019-07-19 02:18:06 +0700

    Update animecon.php

--- animecon.php
+++ animecon.php
@@ -1,6 +1,7 @@
 <?php 
 $id = $_GET['id'];
 $eps = $_GET['eps'];
+$judul = $_GET['judul'];
 $animesingle = "https://api.gdriveplayer.us/v1/anime/id/$id";
 $animesingleget = file_get_contents($animesingle);
 $animesingles = json_decode($animesingleget);
